b1ca44ff4fb2cfb7dd4f395fcc781d23590dbe29
marcofavero3 <marcofavero3@live.com>
2026-04-16 13:22:36 -0300

    - setup projeto

---
+++ src/test/java/com/devdogstudio/kanban_api/KanbanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdogstudio.kanban_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
