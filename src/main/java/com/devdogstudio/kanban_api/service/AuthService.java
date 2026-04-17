3119dffa569533ef90423441a2ae6c74e011f339
marcofavero3 <marcofavero3@live.com>
2026-04-16 23:40:44 -0300

    feat: add authentication endpoints (register and login)

---
+++ src/main/java/com/devdogstudio/kanban_api/service/AuthService.java
@@ -0,0 +1,69 @@
+package com.devdogstudio.kanban_api.service;
+
+import com.devdogstudio.kanban_api.dto.request.LoginRequest;
+import com.devdogstudio.kanban_api.dto.request.RegisterRequest;
+import com.devdogstudio.kanban_api.dto.response.AuthResponse;
+import com.devdogstudio.kanban_api.entity.User;
+import com.devdogstudio.kanban_api.repository.UserRepository;
+import com.devdogstudio.kanban_api.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request) {
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("Email já cadastrado");
+        }
+
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+
+        userRepository.save(user);
+
+        String accessToken = jwtService.generateAcessToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user);
+
+        return AuthResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        String accessToken = jwtService.generateAcessToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user);
+
+        return AuthResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+}
