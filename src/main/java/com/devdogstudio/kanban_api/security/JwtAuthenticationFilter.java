507991abb926066303e67fb40cbfe6b6fb303018
marcofavero3 <marcofavero3@live.com>
2026-04-16 23:22:15 -0300

    feat: add JWT service, user details and user repository

---
+++ src/main/java/com/devdogstudio/kanban_api/security/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.devdogstudio.kanban_api.security;
+
+public class JwtAuthenticationFilter {
+}
---
+++ src/main/java/com/devdogstudio/kanban_api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.devdogstudio.kanban_api.security;
+
+import com.devdogstudio.kanban_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "Usuário não encontrado: " + email
+                ));
+    }
+}
